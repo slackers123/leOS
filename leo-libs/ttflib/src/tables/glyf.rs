4ca80d847748268bb2484866aa784156a63269a1
slackers123 <srekcals@proton.me>
2024-09-21 19:00:54 +0200

    fixed mistake in font parsing

--- leo-libs/ttflib/src/tables/glyf.rs
+++ leo-libs/ttflib/src/tables/glyf.rs
@@ -29,23 +29,15 @@ pub fn get_glyf(
         let instruction_length: u16 = reader.read();
         let instructions: Vec<u8> = read_vec(&mut reader, instruction_length as usize);
 
-        const ON_CURVE_POINT: u8 = 0x01;
-        const X_SHORT_VECTOR: u8 = 0x02;
-        const Y_SHORT_VECTOR: u8 = 0x04;
-        const REPEAT_FLAG: u8 = 0x08;
-        const X_IS_SAME_OR_POSITIVE_X_SHORT_VECTOR: u8 = 0x10;
-        const Y_IS_SAME_OR_POSITIVE_Y_SHORT_VECTOR: u8 = 0x20;
-        const OVERLAP_SIMPLE: u8 = 0x40;
-
-        let mut flags_arr: Vec<u8> = vec![];
+        let mut flags_arr = vec![];
         let mut i = 0;
         loop {
             if i >= num_points {
                 break;
             }
-            let flags = reader.read_byte();
+            let flags = reader.read::<GlyphFlags>();
             //println!("{flags:b}");
-            if (flags & REPEAT_FLAG) != 0 {
+            if flags.repeat_flag {
                 let num = reader.read_byte();
                 for _ in 0..num as u16 + 1 {
                     flags_arr.push(flags);
@@ -59,38 +51,40 @@ pub fn get_glyf(
 
         let mut x_coordinates: Vec<i16> = Vec::with_capacity(flags_arr.len());
         for i in 0..flags_arr.len() {
+            let last = x_coordinates.last().unwrap_or(&0);
             let flags = flags_arr[i];
-            if flags & X_SHORT_VECTOR != 0 {
+            if flags.x_short_vector {
                 let v = reader.read_byte();
-                if flags & X_IS_SAME_OR_POSITIVE_X_SHORT_VECTOR != 0 {
-                    x_coordinates.push(v as i16);
+                if flags.x_is_same_or_positive_x_short_vector {
+                    x_coordinates.push(last + v as i16);
                 } else {
-                    x_coordinates.push(-(v as i16));
+                    x_coordinates.push(last - v as i16);
                 }
             } else {
-                if flags & X_IS_SAME_OR_POSITIVE_X_SHORT_VECTOR != 0 {
-                    x_coordinates.push(*x_coordinates.last().unwrap());
+                if flags.x_is_same_or_positive_x_short_vector {
+                    x_coordinates.push(*last);
                 } else {
-                    x_coordinates.push(reader.read());
+                    x_coordinates.push(last + reader.read::<i16>());
                 }
             }
         }
 
         let mut y_coordinates: Vec<i16> = Vec::with_capacity(flags_arr.len());
         for i in 0..flags_arr.len() {
+            let last = y_coordinates.last().unwrap_or(&0);
             let flags = flags_arr[i];
-            if flags & Y_SHORT_VECTOR != 0 {
+            if flags.y_short_vector {
                 let v = reader.read_byte();
-                if flags & Y_IS_SAME_OR_POSITIVE_Y_SHORT_VECTOR != 0 {
-                    y_coordinates.push(v as i16);
+                if flags.y_is_same_or_positive_y_short_vector {
+                    y_coordinates.push(last + v as i16);
                 } else {
-                    y_coordinates.push(-(v as i16));
+                    y_coordinates.push(last - v as i16);
                 }
             } else {
-                if flags & Y_IS_SAME_OR_POSITIVE_Y_SHORT_VECTOR != 0 {
-                    y_coordinates.push(*x_coordinates.last().unwrap());
+                if flags.y_is_same_or_positive_y_short_vector {
+                    y_coordinates.push(*last);
                 } else {
-                    y_coordinates.push(reader.read());
+                    y_coordinates.push(last + reader.read::<i16>());
                 }
             }
         }
@@ -131,10 +125,44 @@ impl Readable for GlyfHeader {
 
 #[derive(Debug)]
 pub struct GlyphTable {
-    end_pts_of_contours: Vec<u16>,
-    instruction_length: u16,
-    instructions: Vec<u8>,
-    flags: Vec<u8>,
-    x_coordinates: Vec<i16>,
-    y_coordinates: Vec<i16>,
+    pub end_pts_of_contours: Vec<u16>,
+    pub instruction_length: u16,
+    pub instructions: Vec<u8>,
+    pub flags: Vec<GlyphFlags>,
+    pub x_coordinates: Vec<i16>,
+    pub y_coordinates: Vec<i16>,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct GlyphFlags {
+    pub on_curve_point: bool,
+    pub x_short_vector: bool,
+    pub y_short_vector: bool,
+    pub repeat_flag: bool,
+    pub x_is_same_or_positive_x_short_vector: bool,
+    pub y_is_same_or_positive_y_short_vector: bool,
+    pub overlap_simple: bool,
+}
+
+impl Readable for GlyphFlags {
+    fn read(reader: &mut Reader<impl corelib::reader::ByteReader>) -> Self {
+        let flags = reader.read_byte();
+        const ON_CURVE_POINT: u8 = 0x01;
+        const X_SHORT_VECTOR: u8 = 0x02;
+        const Y_SHORT_VECTOR: u8 = 0x04;
+        const REPEAT_FLAG: u8 = 0x08;
+        const X_IS_SAME_OR_POSITIVE_X_SHORT_VECTOR: u8 = 0x10;
+        const Y_IS_SAME_OR_POSITIVE_Y_SHORT_VECTOR: u8 = 0x20;
+        const OVERLAP_SIMPLE: u8 = 0x40;
+
+        Self {
+            on_curve_point: flags & ON_CURVE_POINT != 0,
+            x_short_vector: flags & X_SHORT_VECTOR != 0,
+            y_short_vector: flags & Y_SHORT_VECTOR != 0,
+            repeat_flag: flags & REPEAT_FLAG != 0,
+            x_is_same_or_positive_x_short_vector: flags & X_IS_SAME_OR_POSITIVE_X_SHORT_VECTOR != 0,
+            y_is_same_or_positive_y_short_vector: flags & Y_IS_SAME_OR_POSITIVE_Y_SHORT_VECTOR != 0,
+            overlap_simple: flags & OVERLAP_SIMPLE != 0,
+        }
+    }
 }
