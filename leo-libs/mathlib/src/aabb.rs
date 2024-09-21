6ac68579586bd43ef9fcb969049efe869efdd924
slackers123 <srekcals@proton.me>
2024-09-21 13:54:18 +0200

    added axis aligned boundig box type to math crate

---
+++ leo-libs/mathlib/src/aabb.rs
@@ -0,0 +1,35 @@
+use corelib::types::Float;
+
+use crate::vectors::Vec2;
+
+#[derive(Debug, Clone, Copy)]
+pub struct AABB<T> {
+    pub min: Vec2<T>,
+    pub max: Vec2<T>,
+}
+
+impl AABB<Float> {
+    pub fn include_vec(&mut self, point: &Vec2<Float>) {
+        if point.x < self.min.x {
+            self.min.x = point.x
+        };
+        if point.y < self.min.y {
+            self.min.y = point.y
+        };
+        if point.x > self.max.x {
+            self.max.x = point.x
+        };
+        if point.y > self.max.y {
+            self.max.y = point.y
+        };
+    }
+}
+
+impl Default for AABB<Float> {
+    fn default() -> Self {
+        AABB {
+            min: Vec2::INFINITY,
+            max: Vec2::NEG_INFINITY,
+        }
+    }
+}
