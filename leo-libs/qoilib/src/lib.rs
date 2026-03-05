e96fa122fe86aec4538fef123d1730a727cd5a94
slackers123 <srekcals@proton.me>
2026-03-05 15:03:27 +0100

    working roundtrip qoi

---
+++ leo-libs/qoilib/src/lib.rs
@@ -0,0 +1,294 @@
+use std::io::Write;
+
+use corelib::reader::{ByteReader, Readable, Reader};
+
+pub mod reader;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Pixel {
+    r: u8,
+    g: u8,
+    b: u8,
+    a: u8,
+}
+
+#[derive(Debug)]
+pub struct QoiHeader {
+    magic: [u8; 4],   // magic bytes "qoif"
+    pub width: u32,   // image width in pixels (BE)
+    pub height: u32,  // image height in pixels (BE)
+    pub channels: u8, // 3 = RGB, 4 = RGBA
+    pub colorspace: u8, // 0 = sRGB with linear alpha
+                      // 1 = all channels linear
+}
+
+impl Readable for QoiHeader {
+    fn read(reader: &mut Reader<impl ByteReader>) -> Self {
+        let magic = reader.read();
+
+        assert_eq!(&magic, b"qoif");
+
+        QoiHeader {
+            magic,
+            width: reader.read(),
+            height: reader.read(),
+            channels: reader.read(),
+            colorspace: reader.read(),
+        }
+    }
+}
+
+pub struct QoiWriter<'out, 'src, W: Write> {
+    out: &'out mut W,
+    image: &'src [Pixel],
+    header: QoiHeader,
+    pix_arr: [Pixel; 64],
+    previous_pixel: Pixel,
+    index: usize,
+}
+
+impl<'out, 'src, W: Write> QoiWriter<'out, 'src, W> {
+    pub fn new(header: QoiHeader, image: &'src [Pixel], out: &'out mut W) -> Self {
+        Self {
+            out,
+            image,
+            header,
+            pix_arr: [Pixel {
+                r: 0,
+                g: 0,
+                b: 0,
+                a: 0,
+            }; 64],
+            previous_pixel: Pixel {
+                r: 0,
+                g: 0,
+                b: 0,
+                a: 255,
+            },
+            index: 0,
+        }
+    }
+
+    pub fn write(&mut self) {
+        self.out.write_all(b"qoif").unwrap();
+        self.out
+            .write_all(&self.header.width.to_be_bytes())
+            .unwrap();
+        self.out
+            .write_all(&self.header.height.to_be_bytes())
+            .unwrap();
+        self.out
+            .write_all(&self.header.channels.to_be_bytes())
+            .unwrap();
+        self.out
+            .write_all(&self.header.colorspace.to_be_bytes())
+            .unwrap();
+
+        if self.header.channels == 3 {
+            while self.index < self.image.len() {
+                self.write_next_chunk_rgb();
+            }
+        } else if self.header.channels == 4 {
+            while self.index < self.image.len() {
+                self.write_next_chunk_rgba();
+            }
+        } else {
+            panic!("invalid channel amount");
+        }
+
+        self.out.write_all(&[0, 0, 0, 0, 0, 0, 0, 0x01]).unwrap();
+    }
+
+    pub fn write_next_chunk_rgb(&mut self) {
+        let current_pix = self.image[self.index];
+        if current_pix == self.previous_pixel {
+            let mut cnt = 0;
+
+            while self.index + cnt < self.image.len()
+                && self.image[self.index + cnt] == self.previous_pixel
+                && cnt < 62
+            {
+                cnt += 1;
+            }
+
+            self.index += cnt;
+            self.out
+                .write_all(&[(0b11 << 6) | (cnt - 1) as u8])
+                .unwrap();
+
+            return;
+        }
+
+        if self.color_is_registered(current_pix) {
+            let index_position = (current_pix.r as usize * 3
+                + current_pix.g as usize * 5
+                + current_pix.b as usize * 7
+                + current_pix.a as usize * 11)
+                % 64;
+
+            self.previous_pixel = current_pix;
+            self.out
+                .write_all(&[(0b00 << 6) | index_position as u8])
+                .unwrap();
+            self.index += 1;
+            return;
+        }
+
+        if current_pix.a == self.previous_pixel.a {
+            let r_diff = current_pix.r.wrapping_sub(self.previous_pixel.r) as i8;
+            let g_diff = current_pix.g.wrapping_sub(self.previous_pixel.g) as i8;
+            let b_diff = current_pix.b.wrapping_sub(self.previous_pixel.b) as i8;
+
+            if (r_diff <= 1 && r_diff >= -2)
+                && (g_diff <= 1 && g_diff >= -2)
+                && (b_diff <= 1 && b_diff >= -2)
+            {
+                self.previous_pixel = current_pix;
+                self.reg_pixel(self.previous_pixel);
+
+                self.out
+                    .write_all(&[(0b01 << 6)
+                        | (((r_diff + 2) as u8) << 4)
+                        | (((g_diff + 2) as u8) << 2)
+                        | (((b_diff + 2) as u8) << 0)])
+                    .unwrap();
+                self.index += 1;
+                return;
+            }
+
+            if g_diff <= 31 && g_diff >= -32 {
+                let rg_diff = r_diff - g_diff;
+                let bg_diff = b_diff - g_diff;
+
+                if (rg_diff <= 7 && rg_diff >= -8) && (bg_diff <= 7 && bg_diff >= -8) {
+                    self.previous_pixel = current_pix;
+                    self.reg_pixel(self.previous_pixel);
+                    self.out
+                        .write_all(&[
+                            (0b10 << 6) | (g_diff + 32) as u8,
+                            ((rg_diff + 8) << 4) as u8 | ((bg_diff + 8) << 0) as u8,
+                        ])
+                        .unwrap();
+                    self.index += 1;
+                    return;
+                }
+            }
+        }
+
+        self.previous_pixel = current_pix;
+        self.reg_pixel(self.previous_pixel);
+        self.out
+            .write_all(&[0b11111110, current_pix.r, current_pix.g, current_pix.b])
+            .unwrap();
+
+        self.index += 1;
+    }
+
+    pub fn write_next_chunk_rgba(&mut self) {
+        let current_pix = self.image[self.index];
+        if current_pix == self.previous_pixel {
+            let mut cnt = 0;
+
+            while self.index + cnt < self.image.len()
+                && self.image[self.index + cnt] == self.previous_pixel
+                && cnt < 62
+            {
+                cnt += 1;
+            }
+
+            self.index += cnt;
+            self.out
+                .write_all(&[(0b11 << 6) | (cnt - 1) as u8])
+                .unwrap();
+
+            return;
+        }
+
+        if self.color_is_registered(current_pix) {
+            let index_position = (current_pix.r as usize * 3
+                + current_pix.g as usize * 5
+                + current_pix.b as usize * 7
+                + current_pix.a as usize * 11)
+                % 64;
+
+            self.previous_pixel = current_pix;
+            self.out
+                .write_all(&[(0b00 << 6) | index_position as u8])
+                .unwrap();
+            self.index += 1;
+            return;
+        }
+
+        if current_pix.a == self.previous_pixel.a {
+            let r_diff = current_pix.r.wrapping_sub(self.previous_pixel.r) as i8;
+            let g_diff = current_pix.g.wrapping_sub(self.previous_pixel.g) as i8;
+            let b_diff = current_pix.b.wrapping_sub(self.previous_pixel.b) as i8;
+
+            if (r_diff <= 1 && r_diff >= -2)
+                && (g_diff <= 1 && g_diff >= -2)
+                && (b_diff <= 1 && b_diff >= -2)
+            {
+                self.previous_pixel = current_pix;
+                self.reg_pixel(self.previous_pixel);
+
+                self.out
+                    .write_all(&[(0b01 << 6)
+                        | (((r_diff + 2) as u8) << 4)
+                        | (((g_diff + 2) as u8) << 2)
+                        | (((b_diff + 2) as u8) << 0)])
+                    .unwrap();
+                self.index += 1;
+                return;
+            }
+
+            if g_diff <= 31 && g_diff >= -32 {
+                let rg_diff = r_diff - g_diff;
+                let bg_diff = b_diff - g_diff;
+
+                if (rg_diff <= 7 && rg_diff >= -8) && (bg_diff <= 7 && bg_diff >= -8) {
+                    self.previous_pixel = current_pix;
+                    self.reg_pixel(self.previous_pixel);
+                    self.out
+                        .write_all(&[
+                            (0b10 << 6) | (g_diff + 32) as u8,
+                            ((rg_diff + 8) << 4) as u8 | ((bg_diff + 8) << 0) as u8,
+                        ])
+                        .unwrap();
+                    self.index += 1;
+                    return;
+                }
+            }
+        }
+
+        self.previous_pixel = current_pix;
+        self.reg_pixel(self.previous_pixel);
+        self.out
+            .write_all(&[
+                0b11111111,
+                current_pix.r,
+                current_pix.g,
+                current_pix.b,
+                current_pix.a,
+            ])
+            .unwrap();
+
+        self.index += 1;
+    }
+
+    pub fn reg_pixel(&mut self, pix: Pixel) {
+        let index_position =
+            (pix.r as usize * 3 + pix.g as usize * 5 + pix.b as usize * 7 + pix.a as usize * 11)
+                % 64;
+
+        self.pix_arr[index_position] = pix;
+    }
+
+    #[inline]
+    pub fn color_is_registered(&mut self, pix: Pixel) -> bool {
+        let index_position =
+            (pix.r as usize * 3 + pix.g as usize * 5 + pix.b as usize * 7 + pix.a as usize * 11)
+                % 64;
+
+        self.pix_arr[index_position] == pix
+    }
+}
---
+++ leo-libs/qoilib/src/reader.rs
@@ -0,0 +1,144 @@
+use corelib::reader::{BigEndianReader, Reader};
+
+use crate::{Pixel, QoiHeader};
+
+pub struct QoiReader<'data> {
+    reader: &'data mut Reader<'data, BigEndianReader>,
+    header: QoiHeader,
+    pix_arr: [Pixel; 64],
+    pub result: Vec<Pixel>,
+    previous_pixel: Pixel,
+}
+
+impl<'data> QoiReader<'data> {
+    pub fn new(reader: &'data mut Reader<'data, BigEndianReader>) -> Self {
+        let header: QoiHeader = reader.read();
+        Self {
+            reader,
+            result: Vec::with_capacity((header.height * header.width) as usize),
+            header,
+            pix_arr: [Pixel {
+                r: 0,
+                g: 0,
+                b: 0,
+                a: 0,
+            }; 64],
+            previous_pixel: Pixel {
+                r: 0,
+                g: 0,
+                b: 0,
+                a: 255,
+            },
+        }
+    }
+
+    pub fn read_entire_image(mut self) -> (QoiHeader, Vec<Pixel>) {
+        let pic_size = self.header.width * self.header.height;
+
+        while self.result.len() < pic_size as usize {
+            self.read_chunk();
+        }
+
+        (self.header, self.result)
+    }
+
+    fn reg_new_pix(&mut self, pix: Pixel) {
+        let index_position =
+            (pix.r as usize * 3 + pix.g as usize * 5 + pix.b as usize * 7 + pix.a as usize * 11)
+                % 64;
+
+        self.pix_arr[index_position] = pix;
+    }
+
+    pub fn read_chunk(&mut self) {
+        let first_byte = self.reader.read_byte();
+
+        match first_byte {
+            0b11111110 => {
+                let red = self.reader.read_byte();
+                let green = self.reader.read_byte();
+                let blue = self.reader.read_byte();
+
+                self.previous_pixel = Pixel {
+                    r: red,
+                    g: green,
+                    b: blue,
+                    a: self.previous_pixel.a,
+                };
+
+                self.reg_new_pix(self.previous_pixel);
+                self.result.push(self.previous_pixel);
+            }
+            0b11111111 => {
+                let red = self.reader.read_byte();
+                let green = self.reader.read_byte();
+                let blue = self.reader.read_byte();
+                let alpha = self.reader.read_byte();
+
+                self.previous_pixel = Pixel {
+                    r: red,
+                    g: green,
+                    b: blue,
+                    a: alpha,
+                };
+                self.reg_new_pix(self.previous_pixel);
+                self.result.push(self.previous_pixel);
+            }
+            _ => match first_byte >> 6 {
+                0b00 => {
+                    let index = first_byte & 0b111111;
+
+                    self.previous_pixel = self.pix_arr[index as usize];
+                    self.reg_new_pix(self.previous_pixel);
+                    self.result.push(self.previous_pixel);
+                }
+                0b01 => {
+                    let new_r = (first_byte >> 4) & 0b11;
+                    let new_g = (first_byte >> 2) & 0b11;
+                    let new_b = (first_byte >> 0) & 0b11;
+
+                    self.previous_pixel.r =
+                        (self.previous_pixel.r.wrapping_add(new_r)).wrapping_sub(2);
+                    self.previous_pixel.g =
+                        (self.previous_pixel.g.wrapping_add(new_g)).wrapping_sub(2);
+                    self.previous_pixel.b =
+                        (self.previous_pixel.b.wrapping_add(new_b)).wrapping_sub(2);
+
+                    self.reg_new_pix(self.previous_pixel);
+                    self.result.push(self.previous_pixel);
+                }
+                0b10 => {
+                    let next_byte = self.reader.read_byte();
+                    let diff_green = (first_byte & 0b111111).wrapping_sub(32);
+
+                    let dr_dg = (next_byte >> 4) & 0b1111;
+                    let db_dg = (next_byte >> 0) & 0b1111;
+
+                    let diff_red = diff_green.wrapping_add(dr_dg).wrapping_sub(8);
+                    let diff_blue = diff_green.wrapping_add(db_dg).wrapping_sub(8);
+
+                    self.previous_pixel.r = self.previous_pixel.r.wrapping_add(diff_red);
+                    self.previous_pixel.g = self.previous_pixel.g.wrapping_add(diff_green);
+                    self.previous_pixel.b = self.previous_pixel.b.wrapping_add(diff_blue);
+
+                    self.reg_new_pix(self.previous_pixel);
+                    self.result.push(self.previous_pixel);
+                }
+                0b11 => {
+                    let run_length = first_byte & 0b111111;
+                    assert!(run_length != 0b111111 && run_length != 0b111110);
+
+                    // This handles the edge case of the first chunk being a run
+                    if self.reader.get_pos() == 15 {
+                        self.reg_new_pix(self.previous_pixel);
+                    }
+
+                    for _ in 0..(run_length + 1) {
+                        self.result.push(self.previous_pixel);
+                    }
+                }
+                _ => panic!("invalid qoi file"),
+            },
+        }
+    }
+}
